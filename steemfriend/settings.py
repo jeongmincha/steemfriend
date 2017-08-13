64103e0b2ffb476b39df9f8b5213763b5c15dc71
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-08-14 00:27:30 +0900

    Default Setting for View, Controller for'Friend'

--- steemfriend/settings.py
+++ steemfriend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'friend.apps.FriendConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
