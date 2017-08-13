be508e5265995d4d4cb1ef51e65c143a1bfb107c
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-08-14 03:22:27 +0900

    result.html

--- friend/urls.py
+++ friend/urls.py
@@ -4,8 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from steemfriend import settings
 from . import views
 
+app_name = 'friend'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<account>[a-zA-Z]+)/friends/$', views.friends, name='friends'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- friend/views.py
+++ friend/views.py
@@ -5,3 +5,6 @@ from .models import User
 # Create your views here.
 def index(request):
     return render(request, 'friend/index.html', {})
+
+def friends(request):
+    return render(request, 'friends/result.html', {})
