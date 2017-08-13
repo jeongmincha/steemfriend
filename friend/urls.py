384afcfc31bd863a7ba70fc5a521b3ca89cabcb1
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-08-14 00:57:34 +0900

    Example style.css + index.html

--- friend/urls.py
+++ friend/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from steemfriend import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
