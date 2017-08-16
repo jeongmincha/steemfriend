6d98ecb78f3c818df45459d740db21814844775a
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-08-16 23:46:17 +0900

    Make data_handling package

--- friend/views.py
+++ friend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .data_handling import get_best_friend_factor
+from .data_handling import data_handling
 from .models import User
 
 # Create your views here.
@@ -10,7 +10,7 @@ def index(request):
 def find_friends(request):
     if request.method == "POST":
         account = request.POST['account']
-        friends = get_best_friend_factor(account)
+        friends = data_handling.get_best_friend_factor(account)
         friends = friends[:10]
         return friends_results(request, account, friends)
     else:
